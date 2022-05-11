a9bbf6d5a6b689dae2c0e318118fa08155fd7e9e
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-11 13:14:33 +0200

    feat: display toast message when choice button is clicked

--- app/src/main/java/com/example/dogglers/MainActivity.kt
+++ app/src/main/java/com/example/dogglers/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.dogglers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.dogglers.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  lateinit var binding: ActivityMainBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    setContentView(binding.root)
+
+    binding.apply {
+      verticalBtn.setOnClickListener { makeToast("Vertical button clicked") }
+      horizontalBtn.setOnClickListener { makeToast("Horizontal button clicked") }
+      gridBtn.setOnClickListener { makeToast("Grid button clicked") }
+    }
+
+  }
+
+  private fun makeToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
   }
 }
