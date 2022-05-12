439be388b213c5e40e394dfbc106b96a442a08de
Imron <imron.gamidli@lt.hs-fulda.de>
2022-05-12 10:11:02 +0200

    test(ui): clicking vertical list button displays vertical list

--- app/src/androidTest/java/com/example/dogglers/ButtonTests.kt
+++ app/src/androidTest/java/com/example/dogglers/ButtonTests.kt
@@ -2,8 +2,10 @@ package com.example.dogglers
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
@@ -21,17 +23,23 @@ class ButtonTests {
 
   @Test
   fun vertical_list_button_is_displayed() {
-    onView(ViewMatchers.withId(R.id.vertical_btn))
+    onView(withId(R.id.vertical_btn))
       .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
   }
 
   @Test
   fun horizontal_list_button_is_displayed() {
-    onView(ViewMatchers.withId(R.id.horizontal_btn)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    onView(withId(R.id.horizontal_btn)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
   }
 
   @Test
   fun grid_list_button_is_displayed() {
-    onView(ViewMatchers.withId(R.id.grid_btn)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    onView(withId(R.id.grid_btn)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+  }
+
+  @Test
+  fun clicking_vertical_list_button_displays_vertical_list() {
+    onView(withId(R.id.vertical_btn)).perform(ViewActions.click())
+    onView(withId(R.id.recycler_view)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
   }
 }
